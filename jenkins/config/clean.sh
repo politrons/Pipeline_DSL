b0abc7fe2ad6f807f4d2c58f2097d849257ffd12
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-03-10 09:06:41 +0100

    first commit

--- jenkins/config/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker stop  $(docker ps -a -q)
-docker rm   $(docker ps -a -q)
-docker rmi $(docker images -q)
-
-
-
--- jenkins/init/one_executor.groovy
+++
@@ -1,4 +0,0 @@
-def instance = jenkins.model.Jenkins.getInstance()
-
-println "Use only one master executor"
-instance.setNumExecutors(1)
